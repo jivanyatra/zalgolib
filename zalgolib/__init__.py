7f0d054d370e90c031c1e5d6637503cb1ad38e6f
Yatri Trivedi <jivanyatra@gmail.com>
2022-02-28 18:44:36 +0000

    added version info and made changes for PyPI deployment (adding twine and build as dev requirements)

--- zalgolib/__init__.py
+++ zalgolib/__init__.py
@@ -2,3 +2,5 @@ from .zalgolib import enzalgofy, dezalgofy
 from .diacritics import DOWN_MARKS, DOWN_LEN
 from .diacritics import MID_MARKS, MID_LEN
 from .diacritics import UP_MARKS, UP_LEN
+
+__version__ = "0.2.0"
